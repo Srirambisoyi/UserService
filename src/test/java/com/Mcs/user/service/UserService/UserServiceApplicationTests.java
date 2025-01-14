156ae59f82d511833c2e5376ba2e20a6a57e42ac
Sriram Bisoyi <bisoyisriram@gmail.com>
2025-01-14 20:22:06 +0530

    Userservice first commit

---
+++ src/test/java/com/Mcs/user/service/UserService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mcs.user.service.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
