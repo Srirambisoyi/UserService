7f39c82289210fe0a6369101d0e809389a6fbd16
Sriram Bisoyi <bisoyisriram@gmail.com>
2025-01-19 03:14:10 +0530

    add Restclient in api

--- src/main/java/com/Mcs/user/service/UserService/entities/Rating.java
+++ src/main/java/com/Mcs/user/service/UserService/entities/Rating.java
@@ -1,11 +1,32 @@
 package com.Mcs.user.service.UserService.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Rating {
+    @JsonProperty("ratingid")
     private String ratingId;
+
+    @JsonProperty("userid")
     private String userId;
+
+    @JsonProperty("hotelid")
     private String hotelId;
+
+    @JsonProperty("rating")
     private int rating;
+
+    @JsonProperty("feedback")
     private String feedback;
+
+    private Hotel hotel;
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
 
     public String getRatingId() {
         return ratingId;
@@ -45,5 +66,17 @@ public class Rating {
 
     public void setFeedback(String feedback) {
         this.feedback = feedback;
+    }
+
+    @Override
+    public String toString() {
+        return "Rating{" +
+                "ratingId='" + ratingId + '\'' +
+                ", userId='" + userId + '\'' +
+                ", hotelId='" + hotelId + '\'' +
+                ", rating=" + rating +
+                ", feedback='" + feedback + '\'' +
+                ", hotel=" + hotel +
+                '}';
     }
 }
--- src/main/java/com/Mcs/user/service/UserService/services/impl/UserServiceImpl.java
+++ src/main/java/com/Mcs/user/service/UserService/services/impl/UserServiceImpl.java
@@ -1,19 +1,34 @@
 package com.Mcs.user.service.UserService.services.impl;
 
 import com.Mcs.user.service.UserService.ecxeption.ResourceNotFoundException;
+import com.Mcs.user.service.UserService.entities.Hotel;
+import com.Mcs.user.service.UserService.entities.Rating;
 import com.Mcs.user.service.UserService.entities.User;
+import com.Mcs.user.service.UserService.external.service.HotelService;
 import com.Mcs.user.service.UserService.repository.UserRepository;
 import com.Mcs.user.service.UserService.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
+
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    private HotelService hotelService;
+
+    private Logger logger= LoggerFactory.getLogger(UserServiceImpl.class);
 
     //create user
     @Override
@@ -30,9 +45,32 @@ public class UserServiceImpl implements UserService {
         return userRepository.findAll();
     }
 
+    //get Single User By id
+
     @Override
     public User getUser(String userId) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server !! : " + userId));
+        // Fetch user from the repository
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found: " + userId));
+
+        // Fetch ratings from the Rating service
+        ResponseEntity<Rating[]> responseEntity = restTemplate.getForEntity(
+                "http://RATINGSERVICE/rating/user/" + user.getUserId(), Rating[].class);
+        Rating[] ratingsArray = responseEntity.getBody();
+
+       List<Rating> ratingList=Arrays.stream(ratingsArray).toList();
+      List<Rating> ratings=ratingList.stream().map(rating -> {
+           //api call
+//          ResponseEntity<Hotel> hotelResponseEntity=restTemplate.getForEntity("http://HOTELSERVICE/api/"+rating.getHotelId(), Hotel.class);
+         Hotel hotel=hotelService.getHotel(rating.getHotelId());
+         rating.setHotel(hotel);
+         return rating;
+       }).collect(Collectors.toList());
+
+      user.setRatings(ratings);
+
         return user;
     }
+
+
 }
